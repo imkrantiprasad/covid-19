5d7b43a680139c365e44eec09e808cf5e302fd29
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-05-24 19:21:11 +0530

    Updates

---
+++ precache-manifest.6c0a0f2e345480e866e017626ab6c68d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14d277611fc6bfa21fff7ed0c26b08f",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "f75cbe18c82fc1f1156d",
+    "url": "/covid-19/static/css/main.0858cc2e.chunk.css"
+  },
+  {
+    "revision": "db83009bfbbcb09825f8",
+    "url": "/covid-19/static/js/2.6e2b93bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.6e2b93bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75cbe18c82fc1f1156d",
+    "url": "/covid-19/static/js/main.563c059b.chunk.js"
+  },
+  {
+    "revision": "24320997728edc8dbf96",
+    "url": "/covid-19/static/js/runtime-main.750f9625.js"
+  }
+]);
